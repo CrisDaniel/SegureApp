71a9bc7ac6f50e0bded641dedc0229b1230af15b
pavel mansilla goyas <pavel.mansilla@gmail.com>
2025-06-05 00:25:25 -0500

    logout added

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -28,6 +28,7 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar"
+import { signOut } from "next-auth/react"
 
 export function NavUser({
   user,
@@ -98,7 +99,7 @@ export function NavUser({
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
+            <DropdownMenuItem onClick={async () => await signOut({callbackUrl: "/login"})}>
               <LogOutIcon />
               Log out
             </DropdownMenuItem>
