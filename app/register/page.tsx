360f15cca880983303d3f5e314fc84ffba3cd772
pavel mansilla goyas <pavel.mansilla@gmail.com>
2025-06-04 23:56:05 -0500

    rol added to register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -72,6 +72,7 @@ export default function RegisterPage() {
           name,
           email,
           password,
+          role: 'USER'
         }),
         headers: { "Content-Type": "application/json" }
       });
